bce14a0605bedfec9807a70ace63048727eb2ea6
tatiii2001 <tatiiigomez2001@gmail.com>
2024-04-02 10:21:27 +0100

    Refactoring of the Game class, creating the enum ElementType class to specify the unique options and creating the methods for the verification of the game rules, as well as creating the tests to verify these improvements.

--- src/main/kotlin/com.kata/Game.kt
+++ src/main/kotlin/com.kata/Game.kt
@@ -1,13 +1,15 @@
 package com.kata
 
-class Game(val player1: String, val player2 : String) {
+enum class ElementType{
+    ROCK, PAPER, SCISSORS;
+}
 
-    private val winnerLoserCombinations = listOf(
-        Pair("scissors", "paper"),
-        Pair("paper", "rock"),
-        Pair("rock", "scissors")
-    )
-    fun whoWon(): String {
+class Game(private val player1: String, private val player2 : String) {
+
+    fun whoWins(): String {
+        if(!isTheCorrectElement()){
+            return "It's not the correct element"
+        }
         winnerLoserCombinations.forEach {
             if (it.first == this.player1 && it.second == this.player2){
                 return  "Player 1 win"
@@ -18,5 +20,21 @@ class Game(val player1: String, val player2 : String) {
         }
         return "Draw!!"
     }
+
+    private fun isTheCorrectElement() : Boolean{
+        try {
+            ElementType.valueOf(this.player1.uppercase())
+            ElementType.valueOf(this.player2.uppercase())
+            return true
+        } catch (error : IllegalArgumentException){
+            return false
+        }
+    }
+
+    private val winnerLoserCombinations = listOf(
+        Pair("scissors", "paper"),
+        Pair("paper", "rock"),
+        Pair("rock", "scissors")
+    )
 
 }
--- src/test/kotlin/com.kata/GameShould.kt
+++ src/test/kotlin/com.kata/GameShould.kt
@@ -18,28 +18,28 @@ class GameShould : DescribeSpec({
         it("scissors beats paper"){
             val game = Game("scissors", "paper")
 
-            val result = game.whoWon()
+            val result = game.whoWins()
 
             result.shouldBe("Player 1 win")
         }
         it("paper beats rock"){
             val game = Game("paper", "rock")
 
-            val result = game.whoWon()
+            val result = game.whoWins()
 
             result.shouldBe("Player 1 win")
         }
         it("rock beats scissors"){
             val game = Game("rock", "scissors")
 
-            val result = game.whoWon()
+            val result = game.whoWins()
 
             result.shouldBe("Player 1 win")
         }
         it("it's the same element"){
             val game = Game("rock", "rock")
 
-            val result = game.whoWon()
+            val result = game.whoWins()
 
             result.shouldBe("Draw!!")
         }
@@ -49,9 +49,26 @@ class GameShould : DescribeSpec({
         it("rock beats scissors"){
             val game = Game("scissors", "rock")
 
-            val result = game.whoWon()
+            val result = game.whoWins()
 
             result.shouldBe("Player 2 win")
+        }
+    }
+
+    describe("Player 1 or 2 put the incorrect element"){
+        it("papa beats scissors"){
+            val game = Game("papa", "rock")
+
+            val result = game.whoWins()
+
+            result.shouldBe("It's not the correct element")
+        }
+        it("rock beats papa"){
+            val game = Game("rock", "papa")
+
+            val result = game.whoWins()
+
+            result.shouldBe("It's not the correct element")
         }
     }
 
